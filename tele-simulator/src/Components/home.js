e1872f7dd13be6d5ebebb4ee373673a530d6c155
c2040142 <Nimsam786>
2024-04-18 23:14:08 +0100

    adding placeholder content to home.js

--- tele-simulator/src/Components/home.js
+++ tele-simulator/src/Components/home.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Home = () => {
   return (
-    <div>NICE</div>
-  )
-}
+    <div className="home">
+      <div className="row">
+        <div className="clickable-picture">Placeholder 1</div>
+        <div className="clickable-picture">Placeholder 2</div>
+      </div>
+      <div className="row">
+      </div>
+      <div className="row">
+        <div className="image">Placeholder 3</div>
+        <div className="image">Placeholder 4</div>
+        <div className="image">Placeholder 5</div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
