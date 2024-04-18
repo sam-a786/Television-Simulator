c7930f733d39e3b47378555ea898d451cea9fab1
c2040142 <Nimsam786>
2024-04-18 23:38:25 +0100

    adding to the carousel component

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Project specifically in react(js) to show basic skills
 
 # List of libraries installed
 npm install :
+
+npm install @itseasy21/react-elastic-carousel
--- tele-simulator/src/Components/carousel.js
+++ tele-simulator/src/Components/carousel.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Carousel from "@itseasy21/react-elastic-carousel";
 
+const breakPoints= [
+    { width: 1, itemsToShow: 1 },
+    { width: 550, itemsToShow: 2 },
+    { width: 768, itemsToShow: 3 },
+    { width: 1200, itemsToShow: 4 },
+
+];
 const Carousel = () => {
   return (
-    <div>Carousel</div>
+    <>
+    <div className="carousel">
+    <Carousel breakPoints={breakPoints}>
+        <Item>One</Item>
+        <Item>Two</Item>
+        <Item>Three</Item>
+    </Carousel>
+    </div>
+    </>
   )
 }
 
