dcc0c13ac220d162c8e69ef2549a2bab631c7190
c2040142 <Nimsam786>
2024-04-23 00:43:34 +0100

    functionality as expected all done

--- tele-simulator/src/Components/carousel.js
+++ tele-simulator/src/Components/carousel.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import Carousel from "@itseasy21/react-elastic-carousel";
 import Item from './Item';
-import InfinityImage from '../Images/Infinity war.jpg'; 
+import Thor1 from '../Images/thor1.jpg'; 
+import Thor2 from '../Images/thor2.jpg'; 
+import Thor3 from '../Images/thor3.jpg'; 
+import Thor4 from '../Images/thor4.jpeg'; 
+import av1 from '../Images/avengers1.jpg';
 import "./../styles.css";
 
 const breakPoints = [
@@ -20,11 +24,11 @@ const Carouselcomponent = () => {
     <>
       <div className="carousel" style={carouselStyles}>
         <Carousel breakPoints={breakPoints}>
-          <Item><a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"> <img src={InfinityImage} alt="Infinity War" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
-          <Item><a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"> <img src={InfinityImage} alt="Infinity War" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
-          <Item><a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"> <img src={InfinityImage} alt="Infinity War" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
-          <Item><a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"> <img src={InfinityImage} alt="Infinity War" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
-          <Item><a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"> <img src={InfinityImage} alt="Infinity War" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
+          <Item><a href="https://www.imdb.com/title/tt0800369/?ref_=fn_al_tt_1"> <img src={Thor1} alt="Thor 1" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
+          <Item><a href="https://www.imdb.com/title/tt1981115/?ref_=fn_al_tt_4"> <img src={Thor2} alt="Thor 2" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
+          <Item><a href="https://www.imdb.com/title/tt3501632/?ref_=fn_al_tt_2"> <img src={Thor3} alt="Thor 3" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
+          <Item><a href="https://www.imdb.com/title/tt10648342/?ref_=fn_al_tt_3"> <img src={Thor4} alt="Thor 4" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
+          <Item><a href="https://www.imdb.com/title/tt0848228/?ref_=fn_al_tt_2"> <img src={av1} alt="Avengers assemble" className="carousel-image" style={{ width: '250px', height: '250px'}} /> </a> </Item>
         </Carousel>
       </div>
     </>
--- tele-simulator/src/Components/home.js
+++ tele-simulator/src/Components/home.js
@@ -2,6 +2,16 @@ import React from 'react';
 import Carouselcomponent from './carousel';
 import EndgameImage from '../Images/Endgame.jpeg'; 
 import InfinityImage from '../Images/Infinity war.jpg'; 
+import Ironman1 from '../Images/ironman1.jpg';
+import Ironman2 from '../Images/ironman2.jpg';
+import Ironman3 from '../Images/ironman3.jpg';
+import Cap1 from '../Images/cap1.jpg';
+import Cap2 from '../Images/cap2.jpg';
+import Cap3 from '../Images/cap3.jpeg';
+import Avengers2 from '../Images/avengers2.jpg';
+import DP1 from '../Images/dp1.jpg';
+import DP2 from '../Images/dp2.jpg';
+
 
 const Home = () => {
   return (
@@ -9,56 +19,48 @@ const Home = () => {
       
       
       <div className="row1"> 
-      <a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1">
-            <img src={EndgameImage} alt="Endgame" className="img" style={{ width: '250px', height: '250px', paddingTop: '1100px', paddingBottom: '50px', paddingRight: '300px'  }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '50px' }}/>
-      </a>
+      
+      <a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1"><img src={EndgameImage} alt="Endgame" className="img" style={{ width: '250px', height: '250px', paddingTop: '1100px', paddingBottom: '50px', paddingRight: '300px'  }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt4154756/"><img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '50px' }}/></a>
+      
       </div>
-      
-      
+            
       <div className="row2">
         <Carouselcomponent />
       </div>
       
       
       <div className="row3">
-      <a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1">
-            <img src={EndgameImage} alt="Endgame" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/>
-      </a>
+      
+      <a href="https://www.imdb.com/title/tt0371746/?ref_=fn_al_tt_1"><img src={Ironman1} alt="Ironman1" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt1228705/?ref_=fn_al_tt_3"><img src={Ironman2} alt="Ironman2" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt1300854/?ref_=fn_al_tt_2"><img src={Ironman3} alt="Ironman3" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/></a>
+      
       </div>
       
       
       <div className="row4">
-      <a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1">
-            <img src={EndgameImage} alt="Endgame" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/>
-      </a>
+      
+      <a href="https://www.imdb.com/title/tt0458339/?ref_=fn_al_tt_3"><img src={Cap1} alt="Cap1" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt1843866/?ref_=fn_al_tt_4"><img src={Cap2} alt="Cap2" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt3498820/?ref_=fn_al_tt_2"><img src={Cap3} alt="Cap3" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/></a>
+      
       </div>
 
 
       <div className="row5">
-      <a href="https://www.imdb.com/title/tt4154796/?ref_=fn_al_tt_1">
-            <img src={EndgameImage} alt="Endgame" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/>
-      </a>
-      <a href="https://www.imdb.com/title/tt4154756/">
-            <img src={InfinityImage} alt="Infinity War" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/>
-      </a>
+      
+      <a href="https://www.imdb.com/title/tt2395427/?ref_=fn_al_tt_1"><img src={Avengers2} alt="Avengers2" className="img" style={{ width: '250px', height: '250px', paddingTop: '50px' , paddingBottom: '100px', paddingRight: '75px'  }}/></a>
+
+      <a href="https://www.imdb.com/title/tt1431045/?ref_=fn_al_tt_2"><img src={DP1} alt="Deadpool1" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px', paddingRight: '75px' }}/></a>
+      
+      <a href="https://www.imdb.com/title/tt5463162/?ref_=fn_al_tt_3"><img src={DP2} alt="Deadpool2" className="img" style={{ width: '250px', height: '250px', paddingBottom: '100px' }}/></a>
+      
       </div>
     </div>
   );
